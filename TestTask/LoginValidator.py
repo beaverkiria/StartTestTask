687989552c9b817b691738a75fc13d21428b5045
Kirill Cherepanov <beaverkiria@gmail.com>
2015-10-12 18:46:24 +0300

    Login validator added

---
+++ TestTask/LoginValidator.py
@@ -0,0 +1,23 @@
+__author__ = 'kirillcherepanov'
+
+import re
+
+
+def is_login_valid(login):
+    if not isinstance(login, basestring):
+        raise ValueError('Login must be a string type.')
+
+    p = re.compile(r"""
+        (^[a-z]$)              #One symbol login
+        |(
+        ^[a-z]                 #The first symbol must be a letter
+        [a-z,\d,.,-]{0,18}     #Then letters, digits, dots and dashes
+        [a-z,\d]$              #The last symbol must be a letter or a digit
+        )
+    """, re.IGNORECASE, re.VERBOSE)
+    m = p.match(login)
+    if m:
+        return 1
+    else:
+        return 0
+
